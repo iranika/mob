4b72430832b539ca71b0847d011b4efaed0269f0
iranika <7370486+iranika@users.noreply.github.com>
2020-06-23 00:03:01 +0900

    Updates

---
+++ precache-manifest.4578cbbb6447fb8e66e4e1b7b2686f5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c0d804252e45186c56",
+    "url": "/css/chunk-vendors.dac79669.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "13d21dc1c37b13b4d465f3b0ece3e758",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84523d36cd75e4d505a7",
+    "url": "/js/about.12f4f064.js"
+  },
+  {
+    "revision": "526765978a5ae419dbe0",
+    "url": "/js/app.048f4cbd.js"
+  },
+  {
+    "revision": "08c0d804252e45186c56",
+    "url": "/js/chunk-vendors.c1acf08f.js"
+  },
+  {
+    "revision": "f96553ce6e7a3fd8181ac586c0f8bcce",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
